65e706ca11d34d8452f731e14d8a3ea142ab536f
Stuart <stupop010@hotmail.co.uk>
2019-11-12 11:44:52 +0000

    keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  console.log(process.env);
+  module.exports = require("./prod");
 } else {
   module.exports = require("./dev");
 }
---
+++ server/config/prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  jwt: process.env.JWT,
+  app_id: process.env.APP_ID,
+  app_key: process.env.APP_KEY
+};
